827db02073f66277d3d8f5c5a6f9b21fd68a4304
Vanchos <ivan.ivanov@mail.ru>
2025-02-11 13:26:54 +0300

    Add ROS Workspace

---
+++ ros_workspace/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/vancho/Linux4RoboticistFinal/ros_workspace/src
+# Build directory: /home/vancho/Linux4RoboticistFinal/ros_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ros_workspace/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/vancho/Linux4RoboticistFinal/ros_workspace/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/vancho/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/vancho/catkin_ws/devel;/opt/ros/noetic' != '' else []
