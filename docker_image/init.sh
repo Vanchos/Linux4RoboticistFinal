e26a792b9f5ba390c99fee23baec8d8c3d7af378
Vanchos <ivan.ivanov@mail.ru>
2025-02-13 13:53:59 +0300

    upd

--- docker_image/init.sh
+++ docker_image/init.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-source /opt/ros/noetic/setup.bash
-source /home/${USER}/ros_workspace/devel/setup.bash
+# source /opt/ros/noetic/setup.bash
+# source /home/ros_workspace/devel/setup.bash
 
 # chmod o+w /dev/bus/usb/001/002 
+
+# запуск ноды робота
+# roslaunch wrh_robot wrh.launch
 
 echo "Hello, I'm script, that work in docker!"
 echo "Today is " $(date +"%d.%m.%Y")
